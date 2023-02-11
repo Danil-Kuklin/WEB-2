324520783ac225d68b84535629b65ac8161ca520
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-02-11 14:54:34 +0300

    первый коммит

---
+++ main.js
@@ -0,0 +1,76 @@
+let a = 36;
+let b = 20;
+console.log("задача 1")
+console.log(`${a} км/ч соответствует ${a/3.6} \n${b} м/с соответствует ${b*3.6} км/ч \n`) 
+
+console.log("задача 2")
+a = 7
+b = 5
+let c = 10
+let P = a+b+c;
+if((a + b) > c && (a + c) > b && (c + b) > a){
+    console.log(`треугольник существует`);
+    console.log(`Периметр = ${P}`);
+    console.log(`Площадь = ${Math.sqrt(P/2*(P/2-a)*(P/2-b)*(P/2-c))}`);
+    console.log(`Соотношение = ${P/Math.sqrt(P/2*(P/2-a)*(P/2-b)*(P/2-c))}`);
+
+} else{console.log(`треугольник не существует`);}
+
+/* console.log("задача 3")
+const numb = prompt('Введите число', '')
+if(Number.isNaN(Number(numb))){
+    console.log("вы ввели не число");
+}
+else{
+    for(let i = 0; i <= numb; i++){
+        if(i % 5 == 0){
+            console.log(`${i} fizz buzz`);
+        } else if(i % 2 == 0){
+            console.log(`${i} buzz`);
+        }
+        else{
+            console.log(`${i} fizz`)
+        }
+    }
+} */
+
+console.log("задача 5")
+let myNumb = 5;
+const numb2 = prompt('Введите число', '')
+if(Number.isNaN(Number(numb2))){
+    console.log("вы ввели не число");
+}
+else{
+    while(numb2!=myNumb){
+        
+    }
+}
+
+console.log("задача 6")
+let n = 20;
+let x = 5;
+let y = 3;
+if(n % x ==0 && n % y == 0 ){
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+console.log("задача 7")
+n = 12;
+if (1<=n && n <= 3){
+    console.log(`месяц ${n} => 1 квартал`);
+}
+else if(4 <= n && n <= 6){
+    console.log(`месяц ${n} => 2 квартал`);
+}
+else if (7 <= n && n <= 9){
+    console.log(`месяц ${n} => 3 квартал`);
+}
+else if(10 <= n && n <= 12){
+    console.log(`месяц ${n} => 4 квартал`);
+}
+else{
+    console.log(null);
+}
+
