f616bb457b1d056d1e2c5b6feea52dd8ec175b4e
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-03-09 15:23:22 +0300

    2 лабараторная задача 2

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ function convertSpeed(x, y){
     else{console.log("ошибка");}
 }
 console.log(`${convertSpeed(36,'toKMH')} `);
+
+console.log("задача 2")
+function absValue(x){
+  if(x > 0){
+    return x
+  }
+  else{
+    return x*(-1)
+  }
+}
+
+console.log(absValue(-334));
